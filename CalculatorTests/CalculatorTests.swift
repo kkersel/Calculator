dac9a406963f3c296dced2cb8fd163feb28cd3d3
Alex <x.innv1@gmail.com>
2021-01-22 13:45:22 +0300

    Initial Commit

---
+++ CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,26 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by Кирилл Кузнецов on 18.01.2021.
+//  Copyright © 2021 kuznetsov. All rights reserved.
+//
+
+import XCTest
+@testable import Calculator
+
+class CalculatorTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testCalc() {
+        
+    }
+
+}
